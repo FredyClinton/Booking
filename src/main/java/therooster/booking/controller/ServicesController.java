2601389af0f44df68248a337d01db783f1ea3c8a
FredyClinton <fezeufredy@hotmail.com>
2025-09-12 17:49:14 +0100

    update `create` endpoint in `ServicesController` to return 201 Created with location header using `UriComponentsBuilder`

--- src/main/java/therooster/booking/controller/ServicesController.java
+++ src/main/java/therooster/booking/controller/ServicesController.java
@@ -4,10 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 import therooster.booking.dto.request.CreateServiceDTO;
 import therooster.booking.dto.response.LireServiceDTO;
 import therooster.booking.service.ServicesService;
 
+import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,9 +22,15 @@ public class ServicesController {
 
     @PostMapping
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ResponseEntity<LireServiceDTO> create(@RequestBody CreateServiceDTO dto) {
+    public ResponseEntity<LireServiceDTO> create(@RequestBody CreateServiceDTO dto,
+                                                 UriComponentsBuilder ucb) {
         LireServiceDTO created = servicesService.createService(dto);
-        return ResponseEntity.ok(created);
+        String serviceId = created.getId().toString();
+        URI serviceUrl = ucb
+                .path("/api/bookings/{bookingId}")
+                .buildAndExpand(serviceId)
+                .toUri();
+        return ResponseEntity.created(serviceUrl).build();
     }
 
     @DeleteMapping("/{serviceId}")
